455721d0757a152918187e18c9d5968e2261e56f
grace.mcghie <gracetmcghie@gmail.com>
2021-07-29 10:44:29 +0800

    Test commit

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("Hello World")
+mode = input("What Mode of operation would you like to use (New Order or Daily Summary): ")
+if mode == "new order": #New Order (Default)
+    print("new order")
+elif mode == "daily summary": #Daily Summary
+    print("daily summary")
+else:
+    print("wrong input")
