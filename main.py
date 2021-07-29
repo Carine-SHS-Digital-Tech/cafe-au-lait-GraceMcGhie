3aef32db3dd7974cb6866c4c6980eb7bdc77c684
grace.mcghie <gracetmcghie@gmail.com>
2021-07-29 10:45:47 +0800

    Basic Structure

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 mode = input("What Mode of operation would you like to use (New Order or Daily Summary): ")
-if mode == "new order": #New Order (Default)
+if mode == "new order":         #New Order (Default)
     print("new order")
-elif mode == "daily summary": #Daily Summary
+elif mode == "daily summary":   #Daily Summary
     print("daily summary")
 else:
-    print("wrong input")
+    print("wrong input")        #incorrect input
