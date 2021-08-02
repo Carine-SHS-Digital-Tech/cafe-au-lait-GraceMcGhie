30b1e16e801364db2aae2bd69466b4a03a7031c2
grace.mcghie <gracetmcghie@gmail.com>
2021-08-02 12:29:16 +0800

    Added a nested dictionary and a loop function on the program.

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
 # NESTED DICTIONARY TO HOLD EACH ORDER
-ORDER_DICTIONARY = {'ORDER_ID': {},'TYPE': {} ,
-          'ITEM_1': {},'QTY_1': {},'EXGST_1': {},
-          'ITEM_2': {},'QTY_2': {},'EXGST_2': {},
-          'ITEM_3': {},'QTY_3': {},'EXGST_3': {},
-          'ITEM_4': {},'QTY_4': {},'EXGST_4': {},
-          'ORDER_CUPS': {},'ORDER_GST': {},'ORDER_TAX': {},'ORDER_TOTAL'{}
-}
-while loop = True:  #Infinite looped program
+ORDER_DICTIONARY = {'ORDER_ID': [], 'TYPE': [],
+          'ITEM_1': [], 'QTY_1': [], 'EXGST_1': [],
+          'ITEM_2': [], 'QTY_2': [], 'EXGST_2': [],
+          'ITEM_3': [], 'QTY_3': [], 'EXGST_3': [],
+          'ITEM_4': [], 'QTY_4': [], 'EXGST_4': [],
+          'ORDER_CUPS': [], 'ORDER_GST': [], 'ORDER_TAX': [], 'ORDER_TOTAL': []}
+while True:  #Infinite looped program
     mode = input("What Mode of operation would you like to use (New Order or Daily Summary): ")
 
-    #New Order (Default)
-    if mode == "new order":
+    if mode == "new order": #New Order (Default)
+        print("New Order")
 
-    #Daily Summary
-    elif mode == "daily summary":
-        print("daily summary")
-    #Incorrect input
-    else:
+    elif mode == "daily summary":   #Daily Summary
+        print("Daily Summary")
+
+    else: #Incorrect input
         print("wrong input")
+
