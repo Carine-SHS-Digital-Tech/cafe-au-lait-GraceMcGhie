769fd431b208ef11ea38c108146263224475465f
grace.mcghie <gracetmcghie@gmail.com>
2021-08-03 09:53:46 +0800

    Added EXGST into the loop

--- main.py
+++ main.py
@@ -6,26 +6,26 @@ ORDER_DICTIONARY = {'ORDER_ID': [], 'TYPE': [],
           'ITEM_4': [], 'QTY_4': [], 'EXGST_4': [],
           'ORDER_CUPS': [], 'ORDER_GST': [], 'ORDER_TAX': [], 'ORDER_TOTAL': []}
 PRICES = {'Price_Cappuccino': 3, 'Price_Espresso': 2.25, 'Price_Latte': 2.5, 'Price_Iced_Coffee': 2.5}
-
 while True:  #Infinite looped program
     mode = input("What Mode of operation would you like to use (New Order or Daily Summary): ")
     if mode == "new order": #New Order (Default)
         print("New Order")
         ORDER_DICTIONARY['TYPE'].append(input('Would you like takeaway or dine in (takeaway incurs a 5% surcharge): '))
-        print('4 available items:\n Cappuccino : $3.00 \n Espresso : $2.25 \n Latte : $2.50 \n Iced_Coffee: $2.50')
+        print('4 available items\n\n Cappuccino : $3.00 \n Espresso : $2.25 \n Latte : $2.50 \n Iced_Coffee: $2.50\n\n')
         count = 0
         for x in range(0, 4):
             count = count + 1
             ITEM = (f'ITEM_{count}')
-            ORDER_DICTIONARY[ITEM].append(input('What will be you first line item from the list above: '))
+            IITEM = input('What will be you first line item from the list above: ')
+            ORDER_DICTIONARY[ITEM].append(IITEM)
             QTY = (f"QTY_{count}")
-            ORDER_DICTIONARY[QTY].append(int(input('What quantity of the coffee would you like: ')))
+            IQTY = int(input('What quantity of the coffee would you like: '))
+            ORDER_DICTIONARY[QTY].append(IQTY)
             EXGST = (f"EXGST_{count}")
-            PRICES[
-            ORDER_DICTIONARY[EXGST].append()
+            IEXGST = PRICES[IITEM]*IQTY
+            ORDER_DICTIONARY[EXGST].append(IEXGST)
         print(ORDER_DICTIONARY)
     elif mode == "daily summary":   #Daily Summary
         print("Daily Summary")
-
     else: #Incorrect input
         print("wrong input")
