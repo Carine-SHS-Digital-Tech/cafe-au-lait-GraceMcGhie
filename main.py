21a9ee8aa082660db35b7f8542dbdc51fd8c5a65
grace.mcghie <gracetmcghie@gmail.com>
2021-08-04 11:51:43 +0800

    Added validation to the input of the type of coffee/item for the dictionary

--- main.py
+++ main.py
@@ -5,23 +5,30 @@ ORDER_DICTIONARY = {'ORDER_ID': [], 'TYPE': [],
           'ITEM_3': [], 'QTY_3': [], 'EXGST_3': [],
           'ITEM_4': [], 'QTY_4': [], 'EXGST_4': [],
           'ORDER_CUPS': [], 'ORDER_GST': [], 'ORDER_TAX': [], 'ORDER_TOTAL': []}
-PRICES = {'Price_Cappuccino': 3, 'Price_Espresso': 2.25, 'Price_Latte': 2.5, 'Price_Iced_Coffee': 2.5}
+PRICES = {'cappuccino': 3, 'espresso': 2.25, 'latte': 2.5, 'iced coffee': 2.5}
 while True:  #Infinite looped program
     mode = input("What Mode of operation would you like to use (New Order or Daily Summary): ")
     if mode == "new order": #New Order (Default)
-        print("New Order")
+        print("\nNew Order\n")
         ORDER_DICTIONARY['TYPE'].append(input('Would you like takeaway or dine in (takeaway incurs a 5% surcharge): '))
-        print('4 available items\n\n Cappuccino : $3.00 \n Espresso : $2.25 \n Latte : $2.50 \n Iced_Coffee: $2.50\n\n')
+        print('\n4 available items\n Cappuccino : $3.00 \n Espresso : $2.25 \n Latte : $2.50 \n Iced_Coffee: $2.50\n\n')
         count = 0
         for x in range(0, 4):
             count = count + 1
             ITEM = (f'ITEM_{count}')
-            IITEM = input('What will be you first line item from the list above: ')
-            ORDER_DICTIONARY[ITEM].append(IITEM)
+            IITEM = input('What coffee from the list above would you like: ')
+            if IITEM.lower() in ['cappuccino', 'espresso', 'latte', 'iced coffee']:
+                ORDER_DICTIONARY[ITEM].append(IITEM)
+            else:
+                while IITEM.lower() not in ['cappuccino', 'espresso', 'latte', 'iced coffee']:
+                    IITEM = input('Wrong input.Try again.\nWhat coffee from the list above would you like: ')
+                    IITEM = IITEM.lower()
+                ORDER_DICTIONARY[ITEM].append(IITEM)
             QTY = (f"QTY_{count}")
             IQTY = int(input('What quantity of the coffee would you like: '))
             ORDER_DICTIONARY[QTY].append(IQTY)
             EXGST = (f"EXGST_{count}")
+            IITEM = IITEM.lower()
             IEXGST = PRICES[IITEM]*IQTY
             ORDER_DICTIONARY[EXGST].append(IEXGST)
         print(ORDER_DICTIONARY)
